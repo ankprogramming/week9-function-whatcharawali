45a83f52035e644e7ff10a0524a2a0ab5e31c091
Unknown <whatcharawali17@gmail.com>
2018-01-29 18:03:25 +0700

    เสร็จแล้วข้อที่ 1

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -2,7 +2,30 @@
 
 using namespace std;
 
+void swap(int * a, int * b) {
+    int Arnon;
+    Arnon = * a;
+    * a = * b;
+    * b = Arnon;
+    }
+
 int main() {
+    int a,b,cmd;
+    cin >> a;
+    cin >> b;
+    do{
+        cout << "menu\n1 swap()\n2 quit()" << endl;
+        cin >> cmd;
+        if (cmd == 1){
+                swap(&a,&b);
+                cout << a << b;
+           } else{
+           break;
+           }
+    } while(1);
+     return 0;
+}
+
     /*เขียนโปรแกรมรับค่าจำนวนเต็มบวกจากคีย์บอร์ด 2 จำนวน เก็บไว้ในตัวแปร และสลับค่าข้อมูลระหว่างตัวแปรสองตัวแปรดังกล่าว
     โดยกำหนดให้โปรแกรมสลับค่าข้อมูลจนกว่าผู้ใช้งานต้องการออกจากการทำงานของโปรแกรม
     และแบ่งการทำงานของโปรแกรมเป็นโปรแกรมย่อย
@@ -10,5 +33,4 @@ int main() {
 
 
 
-    return 0;
-}
+
