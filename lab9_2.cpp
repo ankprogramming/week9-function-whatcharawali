dd1d371cd417e08719bcdf6d211f5c164b63690e
Unknown <whatcharawali17@gmail.com>
2018-01-29 18:20:50 +0700

    เสร็จแล้วข้อที่ 2

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
 
 using namespace std;
+float cArea (float r){
+   float area;
+   area = 3.1416 * r * r;
+   return area;
+   }
 
 int main() {
+    float r, cmd;
+    do{
+        cin >> cmd;
+        if (cmd == 1){
+            cin >> r;
+            cout << cArea(r) << endl;
+        } else {
+        break;
+        }
+    } while(1);
     /* เขียนโปรแกรมรับค่ารัศมีวงกลมจากคีย์บอร์ด และหาพื้นที่ของวงกลมเป็นทศนิยม 4 ตำแหน่ง
     โดยกำหนดให้โปรแกรมหาพื้นที่ของวงกลม จนกว่าผู้ใช้งานต้องการออกจากการทำงานของโปรแกรม
     และกำหนดให้แบ่งการทำงานเป็นโปรแกรมย่อยอย่างน้อย 1 ฟังก์ชัน
     */
+
     return 0;
 }
